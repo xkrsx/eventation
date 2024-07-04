2da99ecae064eada9c1b8c6f1607611e5af54abe
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-04 23:12:51 +0200

    Add logout full functionality
    delete session tokens from cookies and DB, redirecting

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,6 +1,20 @@
 import './page.scss';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { getValidSession } from '../../../database/sessions';
 import RegisterForm from './RegisterForm';
 
-export default function RegisterPage() {
+export default async function RegisterPage() {
+  // 1. Check if sessionToken in cookies exists
+  const sessionCookie = cookies().get('sessionToken');
+
+  // 2. Check if the sessionToken from cookie is still valid in DB
+  const session = sessionCookie && (await getValidSession(sessionCookie.value));
+
+  // 3. Redirect home if sessionToken cookie is valid
+  if (session) {
+    redirect('/');
+  }
+  // 4. Redirect to login page if sessionToken cookie is invalid or doesn't exist
   return <RegisterForm />;
 }
