f2fead6e890a8cf9c492dee42d3898bbfaefbaaf
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-09 12:00:01 +0200

    Add secure getUser sql query
    using sessionToken from cookies

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { getValidSession } from '../../../database/sessions';
+import { getUser } from '../../../database/users';
 
 type Props = {
   params: {
