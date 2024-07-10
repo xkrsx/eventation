6a1e21d8a98505232152114f1261f0cc31eb2a96
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-10 15:10:23 +0200

    Move user and event schemas to migrations

--- migrations/00002-createTableEvents.ts
+++ migrations/00002-createTableEvents.ts
@@ -1,4 +1,30 @@
 import { Sql } from 'postgres';
+import { z } from 'zod';
+
+const eventSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: 'Event name must have at least 3 characters.' })
+    .max(255, { message: 'Event name must have maximum 255 characters.' }),
+  userId: z.number(),
+  timeStart: z.date(),
+  timeEnd: z.date(),
+  category: z.string(),
+  location: z.string().min(3).nullable(),
+  latitude: z.string().min(6).nullable(),
+  longitude: z.string().min(6).nullable(),
+  price: z.number().nullable(),
+  description: z
+    .string()
+    .min(3, {
+      message: 'Event description must have at least 3 characters.',
+    })
+    .nullable(),
+  links: z.string().nullable(),
+  images: z.string().nullable(),
+  public: z.boolean(),
+  cancelled: z.boolean(),
+});
 
 export async function up(sql: Sql) {
   await sql`
@@ -9,8 +35,10 @@ export async function up(sql: Sql) {
       time_start timestamp(0) WITHOUT TIME ZONE NOT NULL,
       time_end timestamp(0) WITHOUT TIME ZONE NOT NULL,
       category varchar NOT NULL,
-      location varchar(255) NOT NULL,
-      price integer NOT NULL,
+      location varchar(255) NULL,
+      latitude varchar(50) NULL,
+      longitude varchar(50) NULL,
+      price integer NULL,
       description text NULL,
       links text NULL,
       images text NULL,
