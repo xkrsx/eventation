e264c72c13d2b6a58dc91530fb51911f7ac12b55
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-10 17:51:47 +0200

    Add API functionality for add event

--- app/events/add/page.tsx
+++ app/events/add/page.tsx
@@ -2,6 +2,7 @@ import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { getValidSession } from '../../../database/sessions';
 import { getUser } from '../../../database/users';
+import AddEventForm from './AddEventForm';
 
 export default async function AddEvent() {
   // 1. Check if sessionToken in cookies exists
@@ -24,45 +25,48 @@ export default async function AddEvent() {
     redirect('/');
   }
 
-  return (
-    <div className="wrapper">
-      <div className="event">
-        <h1>Add event</h1>
-        {/* TODO add API functionality */}
-        {/* add picture upload */}
-        {/* fix time editing */}
-        {/* fix location */}
-        {/* fix category */}
-        <form>
-          <label>
-            Name
-            <input />
-          </label>
-          <label>
-            Start time
-            <input type="time" />
-            <input type="date" />
-          </label>
-          <label>
-            End time
-            <input type="time" />
-            <input type="date" />
-          </label>
-          <label>
-            Price
-            <input type="number" />
-          </label>
-          <label>
-            Description
-            <input />
-          </label>
-          <label>
-            Links
-            <input />
-          </label>
-          <button>Add event</button>
-        </form>
-      </div>
-    </div>
-  );
+  return <AddEventForm />;
+
+  //   return (
+  //     <div className="wrapper">
+  //       <div className="event">
+  //         <h1>Add event</h1>
+  //         {/* TODO add API functionality */}
+  //         {/* add picture upload */}
+  //         {/* fix time editing */}
+  //         {/* fix location */}
+  //         {/* fix category */}
+
+  //         <form>
+  //           <label>
+  //             Name
+  //             <input />
+  //           </label>
+  //           <label>
+  //             Start time
+  //             <input type="time" />
+  //             <input type="date" />
+  //           </label>
+  //           <label>
+  //             End time
+  //             <input type="time" />
+  //             <input type="date" />
+  //           </label>
+  //           <label>
+  //             Price
+  //             <input type="number" />
+  //           </label>
+  //           <label>
+  //             Description
+  //             <input />
+  //           </label>
+  //           <label>
+  //             Links
+  //             <input />
+  //           </label>
+  //           <button>Add event</button>
+  //         </form>
+  //       </div>
+  //     </div>
+  //   );
 }
