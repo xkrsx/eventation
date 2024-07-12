e2cc7f8b0fb1143ee0b152e1d10f7e35c6704979
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-12 16:19:59 +0200

    [WiP] Add sort events by date/price on homepage

--- app/HomeEvents.tsx
+++ app/HomeEvents.tsx
@@ -1,20 +1,33 @@
-// 'use client';
+'use client';
+import { useState } from 'react';
+import AllEventsByDate from './events/AllEventsByDate';
+import AllEventsByPrice from './events/AllEventsByPrice';
 
-// import { useState } from 'react';
-// import AllEvents from './events/AllEvents';
-
-// export default function HomeEvents() {
-//   const [sort, setSort] = useState('earliest');
-//   return (
-//     <div>
-//       Sort by:
-//       <form>
-//         <select>
-//           <option onClick={() => setSort('earliest')}>date</option>
-//           <option onClick={() => setSort('')}>price</option>
-//         </select>
-//       </form>
-//       <AllEvents sort={sort} />
-//     </div>
-//   );
-// }
+export default function HomeEvents() {
+  const [sort, setSort] = useState('date');
+  return (
+    <div>
+      <form>
+        <label>
+          <input
+            type="radio"
+            onClick={() => {
+              setSort('date');
+            }}
+          />
+          date
+        </label>
+        <label>
+          <input
+            type="radio"
+            onClick={() => {
+              setSort('price');
+            }}
+          />
+          price
+        </label>
+      </form>
+      <div>{sort === 'date' ? <AllEventsByDate /> : <AllEventsByPrice />}</div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,13 @@
-import { useState } from 'react';
 import AllEventsByDate from './events/AllEventsByDate';
-import AllEventsByPrice from './events/AllEventsByPrice';
+
+// import HomeEvents from './HomeEvents';
 
 export default function Home() {
-  const [sort, setSort] = useState('date');
   return (
     <div className="wrapper">
       <h1>MAIN PAGE</h1>
-      <form>
-        <select>
-          <option onClick={() => setSort('date')}>date</option>
-          <option onClick={() => setSort('price')}>price</option>
-        </select>
-      </form>
-      {sort === 'date' ? <AllEventsByDate /> : <AllEventsByPrice />}
+      {/* <HomeEvents /> */}
+      <AllEventsByDate />
     </div>
   );
 }
