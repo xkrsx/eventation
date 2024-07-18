07d435ce6dbf6b934cde6e8c3989318ffe6bb2c4
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-18 14:01:52 +0200

    Add sample math jest test

--- util/__tests__/users.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@jest/globals';
-import { getUserPublicByIdInsecure, User } from '../../database/users';
-
-test('Getting user by ID, insecure', () => {
-  expect(getUserPublicByIdInsecure(1)).toContainEqual({ user });
-});
