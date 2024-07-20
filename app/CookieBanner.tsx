e2a158857e9f29a231a9d51a9ee9389a74858fbf
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-20 18:36:31 +0200

    Add cookie banner/local storage setup

---
+++ app/CookieBanner.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import './CookieBanner.scss';
+import { useEffect, useState } from 'react';
+import { parseJson } from '../util/json';
+import { getLocalStorage, setLocalStorage } from '../util/localStorage';
+
+export default function CookieBanner() {
+  const [areCookiesAccepted, setAreCookiesAccepted] = useState(false);
+
+  // // In case you are using state variables with multiple different
+  // // possible types
+  // const [areCookiesAccepted, setAreCookiesAccepted] = useState<
+  //   boolean | string
+  // >(false);
+
+  useEffect(() => {
+    const localStorageValue = getLocalStorage('cookiePolicy');
+
+    if (localStorageValue) {
+      setAreCookiesAccepted(parseJson(localStorageValue));
+      // // Another way: Convert string to boolean
+      // setAreCookiesAccepted(Boolean(localStorageValue));
+    } else {
+      setAreCookiesAccepted(false);
+    }
+  }, []);
+
+  return (
+    <div className={`cookieBanner ${areCookiesAccepted ? 'closed' : 'open'}`}>
+      <div>Do you accept the use of Cookies?</div>
+      <button
+        className="button"
+        onClick={() => {
+          setAreCookiesAccepted(true);
+          setLocalStorage('cookiePolicy', JSON.stringify(true));
+        }}
+      >
+        Accept
+      </button>
+    </div>
+  );
+}
