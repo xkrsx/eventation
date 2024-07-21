2331e047c828b02cf6a5ae0cf71ed4d6088fb423
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-21 23:18:37 +0200

    Add delete profile function

--- app/profile/edit/page.tsx
+++ app/profile/edit/page.tsx
@@ -7,14 +7,9 @@ import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import { getValidSession } from '../../../database/sessions';
 import { deleteUser, getUserPublicById } from '../../../database/users';
+import EditProfilePreview from './EditProfilePreview';
 
-type Props = {
-  params: {
-    userId: string;
-  };
-};
-
-export default async function EditProfile(props: Props) {
+export default async function EditProfile() {
   // // 1. Checking if the sessionToken cookie exists
   const sessionCookie = cookies().get('sessionToken');
 
@@ -31,31 +26,11 @@ export default async function EditProfile(props: Props) {
   }
   // // 3. If the sessionToken is valid, allow user to edit their profile
 
-  const profile = await getUserPublicById(
-    session.token,
-    Number(props.params.userId),
-  );
+  const profile = await getUserPublicById(session.token, session.userId);
 
   if (!profile) {
     redirect('/');
   }
 
-  return (
-    <div className="wrapper">
-      <h1>Edit profile</h1>
-      <div className="profile">
-        <h1>User: {profile.username}</h1>
-        <h2>Location: {profile.location}</h2>
-        <h3>Account since: {dayjs(profile.createdAt).format('MM/YYYY')}</h3>
-        <form
-          onSubmit={async (event) => {
-            event.preventDefault();
-            await deleteUser(session.token, Number(props.params.userId));
-          }}
-        >
-          <button>Delete my profile</button>
-        </form>
-      </div>
-    </div>
-  );
+  return <EditProfilePreview profile={profile} />;
 }
