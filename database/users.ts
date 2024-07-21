f894d6601e84ce6079fde0cfe6af62aa19734997
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-21 22:36:28 +0200

    Add delete user query

--- database/users.ts
+++ database/users.ts
@@ -65,22 +65,28 @@ export const updateUser = cache(
   },
 );
 
-export const deleteUsersEventOrganising = cache(
-  async (sessionToken: string, id: number) => {
-    const [event] = await sql<Event[]>`
-      DELETE FROM events USING sessions
-      WHERE
-        sessions.token = ${sessionToken}
-        AND sessions.expiry_timestamp > now()
-        AND sessions.user_id = events.user_id
-        AND expiry_timestamp > now()
-        AND events.id = ${id}
-      RETURNING
-        events.*
-    `;
-    return event;
-  },
-);
+export const deleteUser = cache(async (sessionToken: string, id: number) => {
+  const [user] = await sql<User[]>`
+    DELETE FROM users USING sessions
+    WHERE
+      sessions.token = ${sessionToken}
+      AND sessions.expiry_timestamp > now()
+      AND sessions.user_id = users.id
+      AND expiry_timestamp > now()
+      AND users.id = ${id}
+    RETURNING
+      users.id,
+      users.username,
+      users.full_name,
+      users.location,
+      users.latitude,
+      users.longitude,
+      users.categories,
+      users.email,
+      users.created_at
+  `;
+  return user;
+});
 
 // public profile by username for logged users
 export const getUserPublicByUsername = cache(
