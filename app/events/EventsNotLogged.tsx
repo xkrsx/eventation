4d4059d0aaf135251d717c1b834f2af5febd414d
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-24 18:22:19 +0200

    Update eslint

--- app/events/EventsNotLogged.tsx
+++ app/events/EventsNotLogged.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { getAllEventsSortDateInsecure } from '../../database/events';
 import SingleEventNotLogged from '../common/SingleEvent/SingleEventNotLogged';
 
@@ -7,6 +8,10 @@ export default async function EventsNotLogged() {
   return (
     <div>
       <h1>All events</h1>
+      <Link href="/profile?returnTo=/profile/events">
+        Log or register to see events in your location
+      </Link>
+
       {events.map((event) => {
         return <SingleEventNotLogged key={`id-${event.id}`} event={event} />;
       })}
