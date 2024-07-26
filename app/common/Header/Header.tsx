793c1aab19a44d444dd33a9ced7b29914bf65f3b
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-26 17:04:27 +0200

    Add fav icons

--- app/common/Header/Header.tsx
+++ app/common/Header/Header.tsx
@@ -1,20 +1,22 @@
+import './Header.scss';
 import Image from 'next/image';
 import Link from 'next/link';
-import logoPic from '../../../public/images/logo-150x60.webp';
+import logoPic from '../../../public/images/logo-293-120.webp';
 import Nav from '../Nav/Nav';
 
 export default function Header() {
   return (
     <div className="wrapper">
       <div className="header">
-        <Link href="/">
-          <Image src={logoPic} alt="Eventation logo" />
-        </Link>
-        {' | '}
-        <Link href="/categories">Categories</Link>
-        {' | '}
-        <Link href="/events">Events</Link>
-        {' | '}
+        <div>
+          <Link href="/">
+            <Image src={logoPic} alt="Eventation logo" />
+          </Link>
+          {' | '}
+          <Link href="/categories">Categories</Link>
+          {' | '}
+          <Link href="/events">Events</Link>
+        </div>
         <Nav />
       </div>
     </div>
