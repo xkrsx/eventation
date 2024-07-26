5b41912ad405a3507d534060d2bc33aeaa108c08
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-26 04:38:51 +0200

    Replace process.env secrets with hardcoded to test pusher

--- app/api/infoStream/reactions/route.ts
+++ app/api/infoStream/reactions/route.ts
@@ -1,108 +1,108 @@
-import { cookies } from 'next/headers';
-import { NextRequest, NextResponse } from 'next/server';
+// import { cookies } from 'next/headers';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export type ReactionsResponseBodyPost =
-  | {
-      reaction: { emoji: string };
-    }
-  | {
-      error: string;
-    };
+// export type ReactionsResponseBodyPost =
+//   | {
+//       reaction: { emoji: string };
+//     }
+//   | {
+//       error: string;
+//     };
 
-interface RequestBody {
-  messageId: string;
-}
+// interface RequestBody {
+//   messageId: string;
+// }
 
-export async function POST(
-  request: NextRequest,
-): Promise<NextResponse<ReactionsResponseBodyPost>> {
-  // Task: Create a new reaction for the message
+// export async function POST(
+//   request: NextRequest,
+// ): Promise<NextResponse<ReactionsResponseBodyPost>> {
+//   // Task: Create a new reaction for the message
 
-  // Get the reaction data from the request
-  const body: RequestBody = await request.json();
+//   // Get the reaction data from the request
+//   const body: RequestBody = await request.json();
 
-  // Validate reactions data with zod
-  const result = reactionSchema.safeParse(body);
+//   // Validate reactions data with zod
+//   const result = reactionSchema.safeParse(body);
 
-  // If client sends request body with incorrect data, return a response with a 400 status code to the client
-  if (!result.success) {
-    return NextResponse.json(
-      {
-        error: 'Request does not contain reaction object',
-      },
-      { status: 400 },
-    );
-  }
+//   // If client sends request body with incorrect data, return a response with a 400 status code to the client
+//   if (!result.success) {
+//     return NextResponse.json(
+//       {
+//         error: 'Request does not contain reaction object',
+//       },
+//       { status: 400 },
+//     );
+//   }
 
-  // Checking if the sessionToken cookie exists or get the token from the cookie
-  const sessionTokenCookie = cookies().get('sessionToken');
+//   // Checking if the sessionToken cookie exists or get the token from the cookie
+//   const sessionTokenCookie = cookies().get('sessionToken');
 
-  // Create a reaction
-  const newReaction =
-    sessionTokenCookie &&
-    (await createReaction(
-      sessionTokenCookie.value,
-      Number(body.messageId),
-      result.data.emoji,
-    ));
+//   // Create a reaction
+//   const newReaction =
+//     sessionTokenCookie &&
+//     (await createReaction(
+//       sessionTokenCookie.value,
+//       Number(body.messageId),
+//       result.data.emoji,
+//     ));
 
-  // If the reaction creation fails, return an error
-  if (!newReaction) {
-    return NextResponse.json(
-      {
-        error: 'Reaction not created or access denied creating reactions',
-      },
-      { status: 400 },
-    );
-  }
+//   // If the reaction creation fails, return an error
+//   if (!newReaction) {
+//     return NextResponse.json(
+//       {
+//         error: 'Reaction not created or access denied creating reactions',
+//       },
+//       { status: 400 },
+//     );
+//   }
 
-  // notify all users about the new reaction
-  await pusherServer.trigger(
-    toPusherKey(`message:${Number(body.messageId)}`),
-    'incoming-reaction',
-    {
-      id: newReaction.id,
-      userId: newReaction.userId,
-      messageId: newReaction.messageId,
-      emoji: newReaction.emoji,
-    },
-  );
+//   // notify all users about the new reaction
+//   await pusherServer.trigger(
+//     toPusherKey(`message:${Number(body.messageId)}`),
+//     'incoming-reaction',
+//     {
+//       id: newReaction.id,
+//       userId: newReaction.userId,
+//       messageId: newReaction.messageId,
+//       emoji: newReaction.emoji,
+//     },
+//   );
 
-  // Return the text content of the message
-  return NextResponse.json({
-    reaction: {
-      id: newReaction.id,
-      userId: newReaction.userId,
-      messageId: newReaction.messageId,
-      emoji: newReaction.emoji,
-    },
-  });
-}
+//   // Return the text content of the message
+//   return NextResponse.json({
+//     reaction: {
+//       id: newReaction.id,
+//       userId: newReaction.userId,
+//       messageId: newReaction.messageId,
+//       emoji: newReaction.emoji,
+//     },
+//   });
+// }
 
-export type ReactionsResponseBodyGet =
-  | {
-      reactions: Reaction[];
-    }
-  | {
-      error: string;
-    };
+// export type ReactionsResponseBodyGet =
+//   | {
+//       reactions: Reaction[];
+//     }
+//   | {
+//       error: string;
+//     };
 
-export type Props = {
-  params: { messageId: string };
-};
+// export type Props = {
+//   params: { messageId: string };
+// };
 
-export async function GET(
-  request: NextRequest,
-  { params }: Props,
-): Promise<NextResponse<ReactionsResponseBodyGet>> {
-  // Task: Get reactions to display it on the gameId page
+// export async function GET(
+//   request: NextRequest,
+//   { params }: Props,
+// ): Promise<NextResponse<ReactionsResponseBodyGet>> {
+//   // Task: Get reactions to display it on the gameId page
 
-  const sessionCookie = request.cookies.get('sessionToken');
-  if (!sessionCookie) {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
-  }
+//   const sessionCookie = request.cookies.get('sessionToken');
+//   if (!sessionCookie) {
+//     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+//   }
 
-  const reactions = await getReactionsInsecure(Number(params.messageId));
+//   const reactions = await getReactionsInsecure(Number(params.messageId));
 
-  return NextResponse.json({ reactions });
-}
+//   return NextResponse.json({ reactions });
+// }
