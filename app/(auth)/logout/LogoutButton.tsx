4179413f6cce88c567811cf85ae4fcfe331e9dce
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-27 22:46:49 +0200

    Remove logout button style, add style to header

--- app/(auth)/logout/LogoutButton.tsx
+++ app/(auth)/logout/LogoutButton.tsx
@@ -14,12 +14,12 @@ export default function LogoutButton() {
         }}
       >
         <svg
+          className="menu-icon logout"
           xmlns="http://www.w3.org/2000/svg"
-          height="2.5rem"
           viewBox="0 -960 960 960"
-          width="2.5rem"
-          fill="#0f0c1d"
         >
+          <title id="logoutTitle">Log out button</title>
+          <desc id="logoutDesc">Button to log out from user's profile.</desc>
           <path d="M180-120q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h299v60H180v600h299v60H180Zm486-185-43-43 102-102H360v-60h363L621-612l43-43 176 176-174 174Z" />
         </svg>
       </button>
