7c310676a0f28eb206d40e23b83b27d54a22d59e
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-27 21:34:05 +0200

    Add menu icons and style them

--- app/(auth)/logout/LogoutButton.tsx
+++ app/(auth)/logout/LogoutButton.tsx
@@ -5,14 +5,23 @@ import { logout } from './actions';
 export default function LogoutButton() {
   const router = useRouter();
   return (
-    <form style={{ flexDirection: 'row' }}>
+    <form>
       <button
+        className="logout-button"
         formAction={async () => {
           await logout();
           router.refresh();
         }}
       >
-        logout
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="2.5rem"
+          viewBox="0 -960 960 960"
+          width="2.5rem"
+          fill="#0f0c1d"
+        >
+          <path d="M180-120q-24 0-42-18t-18-42v-600q0-24 18-42t42-18h299v60H180v600h299v60H180Zm486-185-43-43 102-102H360v-60h363L621-612l43-43 176 176-174 174Z" />
+        </svg>
       </button>
     </form>
   );
