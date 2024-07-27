148824fe07c3d57cb6f348aabc6e5e0c2adcb030
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-27 14:00:35 +0200

    Fix showing categories and events in each

--- app/categories/[eventId]/page.tsx
+++ app/categories/[eventId]/page.tsx
@@ -1,9 +1,11 @@
 import { cookies } from 'next/headers';
 import Link from 'next/link';
+import { redirect } from 'next/navigation';
 import { getAllEventsSingleCategoryInsecure } from '../../../database/events';
 import { getValidSession } from '../../../database/sessions';
-import SingleCategoryLogged from '../../common/SingleCategory/SingleCategoryLogged';
-import SingleCategoryNotLogged from '../../common/SingleCategory/SingleCategoryNotLogged';
+import { getCategoryNameById } from '../../../util/categories';
+import SingleEventLogged from '../../common/SingleEvent/SingleEventLogged';
+import SingleEventNotLogged from '../../common/SingleEvent/SingleEventNotLogged';
 
 type Props = {
   params: {
@@ -18,21 +20,39 @@ export default async function SingleCategoryFromParams(props: Props) {
   // // 2. Check if the sessionToken cookie is still valid
   const session = sessionCookie && (await getValidSession(sessionCookie.value));
 
+  const categoryName = getCategoryNameById(Number(props.params.eventId));
+
+  if (categoryName === undefined) {
+    return (
+      <div>
+        <strong>Sorry, category not found.</strong>
+        <Link href="/events/find">Find event.</Link>
+      </div>
+    );
+  }
   // // 3. Get event info
-  const events = await getAllEventsSingleCategoryInsecure(
-    Number(props.params.eventId),
-  );
+  const events = await getAllEventsSingleCategoryInsecure(categoryName);
+  console.log(events);
 
   // // 4. If the sessionToken cookie is invalid or doesn't exist, show single event for unlogged
 
-  // TODO FIX when there are no events in category
-  // // 5. If the sessionToken cookie is valid, show single event for logged
-  return events.length > 1 ? (
+  return (
     <div>
-      <strong>Sorry, category not found.</strong>
-      <Link href="/events/find">Find event.</Link>
+      <h1>{categoryName}</h1>
+      {events.length < 1 ? (
+        <div>
+          <strong>Sorry, no events not found.</strong>
+          <Link href="/events/find">Find event.</Link>
+        </div>
+      ) : (
+        events.map((event) =>
+          events.length >= 1 && session ? (
+            <SingleEventLogged key={`id-${event.id}`} event={event} />
+          ) : (
+            <SingleEventNotLogged key={`id-${event.id}`} event={event} />
+          ),
+        )
+      )}
     </div>
-  ) : (
-    <SingleCategoryLogged events={events} />
   );
 }
