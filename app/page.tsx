cc590d7d3046d7f967a29293ef69477de4e86247
Jakub Markiewicz <markiewicz267@gmail.com>
2024-07-28 06:42:24 +0200

    Add carousel to homepage, change category pics

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,33 @@
 import './page.scss';
-import Image from 'next/image';
+import './common/Carousel/EmblaCarousel.scss';
+import { EmblaOptionsType } from 'embla-carousel';
+// import Image from 'next/image';
 import Link from 'next/link';
+import EmblaCarousel from './common/Carousel/EmblaCarousel';
 
 // import imageUrl from '../public/images/categories/music.webp';
+
+const OPTIONS: EmblaOptionsType = { loop: true, duration: 40 };
+const SLIDE_COUNT = 17;
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 export default function HomePage() {
   return (
     <div className="home-wrapper">
       <h1>We are here for everyone</h1>
-      <Link className="system-link" href="/categories">
-        <div className="home-banner">
-          <Image
+
+      <div className="home-banner">
+        {/* <Image
             className="category-banner"
             src="/images/categories/music.webp"
             alt="Music category"
             fill={true}
-          />
-        </div>
-        <p>Browse all categories</p>
+          /> */}
+
+        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      </div>
+      <Link className="system-link" href="/categories">
+        Browse all categories
       </Link>
     </div>
   );
